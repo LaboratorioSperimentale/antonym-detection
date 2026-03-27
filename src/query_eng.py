534a71b9dc2208682c7dca36914e64a622cbc890
ellepannitto <ellepannitto@gmail.com>
2026-03-27 17:13:58 +0100

    bug fix query_eng and add dump of queries

--- src/query_eng.py
+++ src/query_eng.py
@@ -9,6 +9,7 @@ regular_expression_negative = {}
 
 
 def clean_token(token):
+
 	token = re.sub(r'_.', '', token)
 
 	token = re.sub(r"\s+([,;:!?.])", r"\1", token)
@@ -26,36 +27,30 @@ def valid_sentence_length(text, min_tokens=10, max_tokens=50):
 	n_tokens = len(text.split())
 	return min_tokens < n_tokens < max_tokens
 
-def process_paragraph (current_paragraph, regular_expression_positive, regular_expression_negative):
+def process_paragraph (current_paragraph,
+                    regular_expression_positive,
+                    regular_expression_negative):
 
 	string_paragraph = " ".join(current_paragraph)
- 
+
 	for query in regular_expression_positive:
 		match = re.search(query, string_paragraph)
+
 		if match:
 			X_found = match.group(1)
 			Y_found = match.group(2)
-		# 	print("\nPOSITIVE MATCH")
-		# 	print("query:", query)
-		# 	print("Match:", match.group())
-		# 	print("Paragraph:", string_paragraph)
-   
+
 			query_sentence = rf'[.?!]([^.?!]*)({query})([^.?!]*[.?!])'
 			match = re.search(query_sentence, string_paragraph)
 			x, y, pattern = regular_expression_positive[query]
 			istance = "yes"
-   
 
 			if match:
-
 				extraction = f"{istance}\t{pattern}\t{X_found}\t{Y_found}\t{x} - {y}\t{query}\t{match.group(1)}\t{match.group(2)}\t{match.group(5)}"
 				extraction = clean_token(extraction)
 				if valid_sentence_length(extraction):
 					print(extraction, file = file_output)
 
-
-	
-  
 	for query in regular_expression_negative:
 		match = re.search(query, string_paragraph)
 		if match:
@@ -69,18 +64,13 @@ def process_paragraph (current_paragraph, regular_expression_positive, regular_e
 			query_sentence = rf'[.?!]([^.?!]*)({query})([^.?!]*[.?!])'
 			match = re.search(query_sentence, string_paragraph)
 			x, y, pattern = regular_expression_negative[query]
-			istance = "yes"
+			istance = "no"
 
 			if match:
-   
 				extraction = f"{istance}\t{pattern}\t{X_found}\t{Y_found}\t{x} - {y}\t{query}\t{match.group(1)}\t{match.group(2)}\t{match.group(5)}"
 				extraction = clean_token(extraction)
 				if valid_sentence_length(extraction):
 					print(extraction, file = file_output)
-
-
-
-
 
 def generate_pattern_positive(x, y, prefix, inner, suffix):
 
@@ -90,7 +80,7 @@ def generate_pattern_positive(x, y, prefix, inner, suffix):
 		prefix = prefix.strip().split()
 		for el in prefix:
 			query+=rf'{el}_. '
-   
+
 	query+= rf'({x})_j '
 
 
@@ -117,26 +107,26 @@ def generate_pattern_negative(x, y, prefix, inner, suffix, side):
 		prefix = prefix.strip().split()
 		for el in prefix:
 			query+=rf'{el}_. '
-   
+
 	if side == "left":
-   
+
 		query += rf'(?!{x})(\b\w+)_j '
-	
+
 	if side == "right":
-	
+
 		query+= rf'(\b{x})_j '
 
 	if inner:
 		inner = inner.strip().split()
 		for el in inner:
 			query+= rf'{el}_. '
-   
+
 	if side == "left":
-   
+
 		query+= rf'(\b{y})_j '
-	
+
 	if side == "right":
-	
+
 		query += rf'(?!{y})(\b\w+)_j '
 
 	if suffix:
@@ -159,67 +149,77 @@ def extract_pattern(string):
 
 
 if __name__ == "__main__":
-	
-	input_root = pathlib.Path("/media/CORPORA/CORPORA/COCA/coca-wlp/wlp_news_znw")
-	
+
+	input_root = pathlib.Path("data/coca/")
+
 	all_files = list(input_root.rglob("*.txt"))
 	print(all_files)
+
 	patterns_filename = "data/eng_patterns.txt"
 	seeds_filename = "data/eng_seeds.txt"
+
 	seeds = set()
+	with open(seeds_filename) as fin:
+		for line in fin:
+			linesplit = line.strip().split()
+			sorted_linesplit = sorted(linesplit)
+			seeds.add((sorted_linesplit[0], sorted_linesplit[1]))
 
-	with open ("output_eng.tsv", "w") as file_output:
-		
-		
-		
-		with open(seeds_filename) as fin:
-			for line in fin:
-				linesplit = line.strip().split()
-				sorted_linesplit = sorted(linesplit)
-				seeds.add((sorted_linesplit[0], sorted_linesplit[1]))
+	patterns = {}
 
-		# patterns = set()
-		patterns = {}
+	with open(patterns_filename) as fin:
+		for line in fin:
+			line = line.strip()
+			patterns[extract_pattern(line)]=line
 
-		with open(patterns_filename) as fin:
-			for line in fin:
-				line = line.strip()
-				# patterns.add(extract_pattern(line))
-				patterns[extract_pattern(line)]=line
+	with open("data/eng_queries.txt", "w") as file_queries:
 
+		print("class\tpattern\tX\tY\tquery", file=file_queries)
 
 		for pattern in patterns:
 			for x, y in seeds:
-				regular_expression_positive[((generate_pattern_positive(x, y, *pattern)))] = (x, y, patterns[pattern])
-				# print(generate_pattern(x, y, *pattern))
-				regular_expression_positive[((generate_pattern_positive(y, x, *pattern)))] = (x, y, patterns[pattern])
-				# print(generate_pattern(y, x, *pattern))
-				regular_expression_negative[((generate_pattern_negative(x, y, *pattern, "left")))] = (x, y, patterns[pattern])
-				# print(generate_pattern_negative(x, y, *pattern, "left"))
-				regular_expression_negative[((generate_pattern_negative(x, y, *pattern, "right")))] = (x, y, patterns[pattern])
-				# print(generate_pattern_negative(x, y, *pattern, "right"))
-				regular_expression_negative[((generate_pattern_negative(y, x, *pattern, "left")))] = (x, y, patterns[pattern])
-				# print(generate_pattern_negative(y, x, *pattern, "left"))
-				regular_expression_negative[((generate_pattern_negative(y, x, *pattern, "right")))] = (x, y, patterns[pattern])
-				# print(generate_pattern_negative(y, x, *pattern, "right"))
-		
+				positive_one = generate_pattern_positive(x, y, *pattern)
+				positive_two = generate_pattern_positive(y, x, *pattern)
 
-		pbar = tqdm(all_files)	
+				regular_expression_positive[positive_one] = (x, y, patterns[pattern])
+				regular_expression_positive[positive_two] = (x, y, patterns[pattern])
+
+				negative_one = generate_pattern_negative(x, y, *pattern, "left")
+				negative_two = generate_pattern_negative(x, y, *pattern, "right")
+				negative_three = generate_pattern_negative(y, x, *pattern, "left")
+				negative_four = generate_pattern_negative(y, x, *pattern, "right")
+
+				regular_expression_negative[negative_one] = (x, y, patterns[pattern])
+				regular_expression_negative[negative_two] = (x, y, patterns[pattern])
+				regular_expression_negative[negative_three] = (x, y, patterns[pattern])
+				regular_expression_negative[negative_four] = (x, y, patterns[pattern])
+
+				print(f"yes\t{patterns[pattern]}\t{x}\t{y}\t{positive_one}", file=file_queries)
+				print(f"yes\t{patterns[pattern]}\t{x}\t{y}\t{positive_two}", file=file_queries)
+				print(f"no\t{patterns[pattern]}\t{x}\t{y}\t{negative_one}", file=file_queries)
+				print(f"no\t{patterns[pattern]}\t{x}\t{y}\t{negative_two}", file=file_queries)
+				print(f"no\t{patterns[pattern]}\t{x}\t{y}\t{negative_three}", file=file_queries)
+				print(f"no\t{patterns[pattern]}\t{x}\t{y}\t{negative_four}", file=file_queries)
+
+	with open ("data/output_eng.tsv", "w") as file_output:
+
+		pbar = tqdm(all_files)
 		for file in pbar:
 			pbar.set_description(file.stem)
+
 			with open(file, "r", encoding="latin-1") as f:
+
 				for riga in tqdm(f, desc="processing_file"):
-					riga = riga.strip()
-					
-					riga = riga.split("\t")
-			
+
+					riga = riga.strip().split("\t")
+
 					if len(riga) < 4:
 						continue
-					
-					elif riga[2] == "<p>":
-						
-						process_paragraph(current_paragraph, regular_expression_positive, regular_expression_negative)
+
+					if riga[2] == "<p>":
+						process_paragraph(current_paragraph,
+								regular_expression_positive,
+								regular_expression_negative)
 						current_paragraph = []
-				
-					else:            
+					else:
 						current_paragraph.append(riga[1] + "_" + riga[3][0])
